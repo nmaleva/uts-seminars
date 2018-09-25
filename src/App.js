c87b2e79b1a97d0df441c3da99e38e00ca9edf96
Nelly M <nmaleva@gmail.com>
2018-09-25 16:44:55 +1000

    Add Firebase components & authentication

--- src/App.js
+++ src/App.js
@@ -3,32 +3,96 @@ import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import { simpleAction } from './actions/simpleAction'
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+var config = {
+  apiKey: "AIzaSyDKwSoUn-wqWF3rpKiqupGUKnzTGegZjbQ",
+  authDomain: "uts-seminars.firebaseapp.com",
+  databaseURL: "https://uts-seminars.firebaseio.com",
+  storageBucket: "uts-seminars.appspot.com",
+};
+firebase.initializeApp(config);
+
+// ui.start('#firebaseui-auth-container', {
+//   signInOptions: [
+//     firebase.auth.EmailAuthProvider.PROVIDER_ID
+//   ],
+//   // Other config options...
+// });
 
 class App extends Component {
   simpleAction = (event) => {
     this.props.simpleAction();
    }
 
+   // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
  render() {
-  return (
-   <div className="App">
-    <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" />
-     <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-     Malek Sucks
-     <button onClick={this.simpleAction}>I agree</button>
-     <pre>
-      {
-        JSON.stringify(this.props)
-      }
-      </pre>
-    </p>
-   </div>
-  );
- }
+  if (!this.state.isSignedIn) {
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+      </div>
+    );
+  }
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+      </div>
+    );
+  }
+  // return (
+  //  <div className="App">
+
+  //   {/* <p className="App-intro">
+
+  //    <button onClick={this.simpleAction}>I agree</button>
+  //    <pre>
+  //     {
+  //       JSON.stringify(this.props)
+  //     }
+  //     </pre>
+  //   </p> */}
+  //  </div>
+  // );
 }
+
 const mapStateToProps = state => ({
   ...state
  })
