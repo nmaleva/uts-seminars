37e49013d042940e566ea3d732f54022aebddba5
Nelly M <nmaleva@gmail.com>
2018-10-02 15:47:04 +1000

    Clean up Home.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,8 @@
 import React, { Component } from 'react'
-import SeminarsTable from "./seminarsTable"
-//import Db from "../firebase/database"
-import {addSeminarToFirebase, getSeminarsFromFirebase} from "../firebase/base"
-import Button from '@material-ui/core/Button/Button';
-import store from '../store'
-
 import ReactDOM from 'react-dom'
-
 import {connect} from 'react-redux'
-
-// var array = [];
-
-// function handleClick(e) {
-//     e.preventDefault();
-//     addSeminarToFirebase("test");
-// }
-
-// function getData(e){
-//     array = getSeminarsFromFirebase();
-
-// }
-
-
-// const Home = () => {
-
-//     class SSeminars extends React.Component{
-//         render() {
-//             return <div> ${array[0]} </div>;
-//         }
-//     }
-
-//     return (
-//         <div className="container">
-//             <p>Welcome, this is the homepage!</p>
-//             <SeminarsTable/>
-//             <SSeminars />
-//             Sup <Button onClick={getData()}> read </Button>
-//             <Button onClick={handleClick} > aewfwe </Button>
-//         </div>
-
-//     )
-// }
+import SeminarsTable from "./seminarsTable"
+import Button from '@material-ui/core/Button/Button';
 
 class Home extends Component {
     render() {
@@ -54,8 +16,6 @@ class Home extends Component {
         )
     }
 }
-
-//Db.addSeminarToFirebase();
 
 //Grabbing Project objects from the store 
 const mapStateToProps = (state) => {
