a0943df480df4b5fe12ed893377037595b9f7f3c
Nelly M <nmaleva@gmail.com>
2018-10-03 19:25:23 +1000

    Fix Form Styling

--- src/components/CreateSeminarForm.js
+++ src/components/CreateSeminarForm.js
@@ -12,6 +12,7 @@ import InputLabel from "@material-ui/core/InputLabel"
 import moment from 'moment'
 import hosts from '../data/hosts'
 import venues from '../data/venues'
+import { autofill } from 'redux-form';
 
 const styles = theme => ({
     formControl: {
@@ -69,51 +70,57 @@ class CreateSeminarForm extends Component {
         console.log(this.props)
 
         return (
-            <div className="container">
-                <form >
+            <div>
+                <form style={{width: '100%' }}>
                     <h1> Create Seminar </h1>
-                    <FormControl className={classes.formControl}>
-                       
-                        <TextField required id="title" label="Enter Title" value={this.state.title} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <TextField id="abstract" label="Enter Abstract" multiline value={this.state.abstract} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <TextField id="speaker" label="Enter Speaker" value={this.state.speaker} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor="host-id">Select Host</InputLabel>
-                        <Select value={this.state.host} onChange={this.handleSelectChange}
-                            inputProps={{
-                                name: 'host',
-                                id: 'host-id'
-                            }}>
-                            <MenuItem value=""><em>None</em></MenuItem>
-                            {hostItems}
-                        </Select>
-                    </FormControl>
+                    <div>
+                        <FormControl className={classes.formControl}>       
+                            <TextField style={{width: 500}} required id="title" label="Enter Title" value={this.state.title} onChange={this.handleChange}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <TextField id="speaker" label="Enter Speaker"  style={{width: 300}}  value={this.state.speaker} onChange={this.handleChange}/>
+                        </FormControl>
+                    </div>
+                    <div>
+                        <FormControl className={classes.formControl}>
+                            <TextField required id="abstract" label="Enter Abstract" style={{width: 800}} multiline value={this.state.abstract} onChange={this.handleChange}/>
+                        </FormControl>
+                    </div>
+                    <div>
+                        <div>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="host-id">Select Host</InputLabel>
+                                <Select value={this.state.host} onChange={this.handleSelectChange}
+                                    inputProps={{
+                                        name: 'host',
+                                        id: 'host-id'
+                                    }}>
+                                    <MenuItem value=""><em>None</em></MenuItem>
+                                    {hostItems}
+                                </Select>
+                            </FormControl>
 
-                    <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor="venue">Select Venue</InputLabel>
-                        <Select name="venue" value={this.state.venue} onChange={this.handleSelectChange}>
-                            <MenuItem value=""><em>None</em></MenuItem>
-                            {venueItems}
-                        </Select>
-                    </FormControl>
-                    <br/>
-                    <FormControl className={classes.formControl}>
-                        <TextField id="date" type="date" label="Choose Date" value={this.state.date} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <TextField id="time" type="time" label="Choose Time" value={this.state.time} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <TextField id="duration" type="number" label="Enter Duration in Minutes"  value={this.state.duration} onChange={this.handleChange}/>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <Button disabled={!isEnabled} variant="contained" color="primary" className={classes.button} onClick={this.handleSubmit}> Submit </Button>
-                    </FormControl>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="venue">Select Venue</InputLabel>
+                                <Select name="venue" value={this.state.venue} onChange={this.handleSelectChange}>
+                                    <MenuItem value=""><em>None</em></MenuItem>
+                                    {venueItems}
+                                </Select>
+                            </FormControl>
+                            <FormControl className={classes.formControl}>
+                                <TextField id="date" type="date" label="Choose Date" value={this.state.date} onChange={this.handleChange}/>
+                            </FormControl>
+                        </div>
+                        <FormControl className={classes.formControl}>
+                            <TextField id="time" type="time" label="Choose Time" value={this.state.time} onChange={this.handleChange}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <TextField id="duration" type="number" label="Enter Duration in Minutes"  value={this.state.duration} onChange={this.handleChange}/>
+                        </FormControl><br/><br/>
+                        <FormControl className={classes.formControl}>
+                            <Button disabled={!isEnabled} variant="contained" color="primary" className={classes.button} onClick={this.handleSubmit}> Submit </Button>
+                        </FormControl>
+                    </div>
                 </form>
             </div>
         )
