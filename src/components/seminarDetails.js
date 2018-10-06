31e7f31d6887a4202784bf1d95659083a5286d31
Nelly M <nmaleva@gmail.com>
2018-10-06 15:57:53 +1000

    Add data showing from db for Seminar Details

--- src/components/seminarDetails.js
+++ src/components/seminarDetails.js
@@ -2,22 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {connect} from 'react-redux'
 import RegistrationForm from './registrationForm'
-
-//Grabbing Project objects from the store 
-const mapStateToProps = (state) => {
-    return {
-        seminars: state.seminar.seminars
-    }
-}
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const SeminarDetails = (props) => {
 
     //grab ID of seminar from url parameters
     const id = props.match.params.id
-    console.log(props.seminars);
+    console.log(props);
+    const {seminar} = props;
 
     //Grab seminar from seminar list in state
-    const seminar = props.seminars.find(seminar => id == seminar.id)
+   // const seminar = props.seminars.find(seminar => id == seminar.id)
     console.log(seminar);
     if(seminar != null) {
         return (
@@ -42,5 +38,19 @@ const SeminarDetails = (props) => {
     }
 }
 
-
-export default connect(mapStateToProps)(SeminarDetails)
+const mapStateToProps = (state, ownProps) => {
+    console.log(state);
+    const id = ownProps.match.params.id;
+    const seminars = state.firestore.data.seminars;
+    const seminar = seminars ? seminars[id] : null
+    return {
+        seminar: seminar
+        //seminars: (state.firestore.ordered.seminars != undefined) ? state.firestore.ordered.seminars : []
+    }
+}
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'seminars' }
+    ])
+)(SeminarDetails)
